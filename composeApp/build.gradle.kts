4fe71cf5811172a0e7ded2e06fb1ad90b5238f90
FrolovIlia <ilia_frolov@list.ru>
2026-01-10 20:25:04 +0300

    Скорректировал длину упражнений

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -73,8 +73,8 @@ android {
         applicationId = "com.pixelrabbit.oculi"
         minSdk = 24
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.30.0"
+        versionCode = 31
+        versionName = "1.31.0"
     }
 
     packaging {
--- composeApp/src/commonMain/kotlin/com/pixelrabbit/oculi/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/com/pixelrabbit/oculi/AppConfig.kt
@@ -1,7 +1,7 @@
 package com.pixelrabbit.oculi
 
 object AppConfig {
-    const val VERSION_NAME = "1.30.0"
-    const val VERSION_CODE = 30
+    const val VERSION_NAME = "1.31.0"
+    const val VERSION_CODE = 31
     const val APP_NAME = "Oculi"
 }
